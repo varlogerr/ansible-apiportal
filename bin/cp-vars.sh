9f45515fc7f6cd88b29877a01310a38c863f21f5
Varlog Err <varlog.err@gmail.com>
2021-08-11 13:14:22 +0300

    create cp-vars script

---
+++ bin/cp-vars.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env bash
+
+THE_SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}")"
+THE_SCRIPT_DIR="$(cd "$(dirname "${THE_SCRIPT_PATH}")" &> /dev/null && pwd)"
+THE_SAMPLES_DIR="$(realpath "${THE_SCRIPT_DIR}/../samples")"
+
+print_help() {
+  echo "Copy vars file. Available options:"
+
+  while read -r l; do
+    [[ -n "${l}" ]] && echo "  ${l}"
+  done <<< "
+    -o, --output    Output target file. Required.
+    -h, -?, --help  Print this help.
+  "
+}
+
+# collect options
+while :; do
+  case "${1}" in
+    -h|-\?|--help)
+      print_help
+      exit
+      ;;
+
+    --output=?*)
+      TARGET="${1#*=}"
+      ;;
+    --output|-o)
+      TARGET="${2}"
+      shift
+      ;;
+    *)
+      break
+      ;;
+  esac
+
+  shift
+done
+
+if [[ -z "${TARGET}" ]]; then
+  echo "Output target is required!"
+  echo
+  print_help
+  exit 1
+fi
+
+TARGET_DIR="$(dirname "${TARGET}")"
+
+if [[ ! -d "${TARGET_DIR}" ]]; then
+  mkdir "${TARGET_DIR}"
+fi
+
+if [[ ! -f "${TARGET}" ]]; then
+  touch "${TARGET}"
+fi
+
+TARGET="$(realpath "${TARGET}")"
+
+sample_content="$(cat "${THE_SAMPLES_DIR}/vars.yml")"
+target_content="$(cat "${TARGET}")"
+target_vars="$(grep -P '^[^\#|^\s|^\-].*' <<< "${target_content}")"
+
+# compare source content with target content
+# and output to the target file
+while IFS= read -r l; do
+  # doesn't start with '#', '-' or space
+  # grab everything up to first ':'
+  var_name="$(
+    grep -P '^[^\#|^\s|^\-].*' <<< "${l}" \
+    | grep -Po '^[^:]+'
+  )"
+
+  if [[ -z "${var_name}" ]]; then
+    # it's not a var, print out directly
+    echo "${l}"
+    continue;
+  fi
+
+  var_found="$(grep -- "^${var_name}:" <<< "${target_vars}")"
+  if [[ -n "${var_found}" ]]; then
+    echo "${var_found}"
+    continue
+  fi
+
+  echo "${l}"
+done <<< "${sample_content}" > "${TARGET}"
